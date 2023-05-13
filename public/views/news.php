5d4158adf3a7a1a2180f29042c9906a66df2dc09
maja <majamitic774@gmail.com>
2023-05-14 00:22:30 +0200

    Style for detele and edit

--- public/views/news.php
+++ public/views/news.php
@@ -1,15 +1,13 @@
 <!-- category -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mx-auto">
+<nav class="navbar navbar-expand-lg navbar-light nav-categories">
+    <div class="container-fluid">
+        <ul class="navbar-nav mx-auto">
+            <?php foreach ($category as $cat) : ?>
                 <li class="nav-item">
-                    <?php foreach ($category as $cat) : ?>
-                        <a href="?page=news&category=<?php echo $cat['id'] ?>"><?= $cat['name'] ?></a>
-                    <?php endforeach; ?>
+                    <a href="?page=news&category=<?php echo $cat['id'] ?>" class="nav-link"><?= $cat['name'] ?></a>
                 </li>
-            </ul>
-        </div>
+            <?php endforeach; ?>
+        </ul>
     </div>
 </nav>
 <!-- and category  -->
@@ -28,25 +26,30 @@
         <?php foreach ($all_news as $news) : ?>
             <div class="col-md-4">
                 <div class="card">
-                    <img class="card-img-top" src="<?= BASE_URL ?>storage/images/<?= $news['image'] ?>" alt="Slika vesti">
+                    <a href="<?= BASE_URL ?>/index.php?page=single-news&news_id=<?php echo $news['id'] ?>">
+                        <img class="card-img-top" src="<?= BASE_URL ?>storage/images/<?= $news['image'] ?>" alt="Slika vesti">
+                    </a>
                     <div class="card-body">
-                        <h5 class="card-title"><a href="#"><?= $news['title'] ?></a></h5>
+                        <a href="<?= BASE_URL ?>/index.php?page=single-news&news_id=<?php echo $news['id'] ?>" class="link-dark text-decoration-none">
+                            <h5 class="card-title"><?= $news['title'] ?></h5>
+                        </a>
                         <small class="text-muted">Created: <?= $news['created_at'] ?></small>
-                        <form action="<?= BASE_URL . 'index.php' ?>" method="POST">
-                            <?php if (isset($_SESSION['token'])) : ?>
-                                <input type="hidden" name="token" value="<?php echo $_SESSION['token'] ?>">
-                                <input type="hidden" name="id" value="<?= $news['id'] ?>">
-                                <?php if (false == $auth->isAdmin()) : ?>
-                                <?php else : ?>
-                                    <button type="submit" name="delete-news">delete</button><br />
+                        <div class="d-flex">
+                            <form action="<?= BASE_URL . 'index.php' ?>" method="POST">
+                                <?php if (isset($_SESSION['token'])) : ?>
+                                    <input type="hidden" name="token" value="<?php echo $_SESSION['token'] ?>">
+                                    <input type="hidden" name="id" value="<?= $news['id'] ?>">
+                                    <?php if (false == $auth->isAdmin()) : ?>
+                                    <?php else : ?>
+                                        <button type="submit" name="delete-news" class="btn btn-danger">delete</button><br />
+                                    <?php endif; ?>
                                 <?php endif; ?>
+                            </form>
+                            <?php if ($auth != False && false == $auth->isAdmin()) : ?>
+                            <?php else : ?>
+                                <a href="<?= BASE_URL ?>index.php?page=update-news&news_id=<?php echo $news['id'] ?>" class="btn btn-primary">edit</a>
                             <?php endif; ?>
-                        </form>
-                        <a href="<?= BASE_URL ?>/index.php?page=single-news&news_id=<?php echo $news['id'] ?>">single news</a>
-                        <?php if ($auth != False && false == $auth->isAdmin()) : ?>
-                        <?php else : ?>
-                            <a href="<?= BASE_URL ?>index.php?page=update-news&news_id=<?php echo $news['id'] ?>">update</a>
-                        <?php endif; ?>
+                        </div>
                     </div>
                 </div>
             </div>
