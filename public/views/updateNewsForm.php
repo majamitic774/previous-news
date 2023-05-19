e72fb19814d2b821378f03c96a3598984ffc76fc
maja <majamitic774@gmail.com>
2023-05-19 13:22:30 +0200

    Style for edit news form

--- public/views/updateNewsForm.php
+++ public/views/updateNewsForm.php
@@ -1,8 +1,19 @@
-<form action="<?= BASE_URL . 'index.php' ?>" method="POST" enctype="multipart/form-data">
-    <input type="hidden" name="token" value="<?php echo $_SESSION['token'] ?>">
-    <input type="hidden" name="id" value="<?php echo $_GET['news_id'] ?>"> <br />
-    title<input type="text" name="title" value="<?= $news['title'] ?>"><br />
-    body<input type="text" name="body" value="<?= $news['body'] ?>"><br />
-    img<input type="file" name="image"><br />
-    <button type="submit" name="updateNews"> Update</button>
-</form>
+<div class="container-short">
+    <form action="<?= BASE_URL . 'index.php' ?>" method="POST" enctype="multipart/form-data">
+        <input type="hidden" name="token" value="<?php echo $_SESSION['token'] ?>">
+        <input type="hidden" name="id" value="<?php echo $_GET['news_id'] ?>"> <br />
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control" name="title" value="<?= $news['title'] ?>">
+        </div>
+        <div class="mb-3">
+            <label for="body" class="form-label">Body</label>
+            <input type="text" class="form-control" name="body" value="<?= $news['body'] ?>">
+        </div>
+        <div class="mb-3">
+            <label for="image" class="form-label">Image</label>
+            <input type="file" class="form-control" name="image"><br />
+        </div>
+        <button type="submit" name="updateNews" class="btn btn-primary">Edit</button>
+    </form>
+</div>
