38fb86c51b8f29749c04ec9e75b971827d6fc163
maja <majamitic774@gmail.com>
2023-05-20 19:42:41 +0200

    Edit form for updated comments

--- public/views/updateCommentForm.php
+++ public/views/updateCommentForm.php
@@ -8,13 +8,17 @@ $comment = $comment->findById($commentId);
 
 ?>
 
-<body>
+<div class="container-short">
     <form action="<?= BASE_URL . 'index.php' ?>" method="POST">
         <input type="hidden" name="token" value="<?php echo $_SESSION['token'] ?>">
         <input type="hidden" name="comment_id" value="<?php echo $commentId; ?>"><br />
         <input type="hidden" name="news_id" value="<?php echo $_GET['news_id'] ?>"><br />
         <input type="hidden" name="user-commented-email" value="<?php echo $_GET['user-commented-email'] ?>"><br />
-        body<input type="text" name="body" value="<?= $comment['body'] ?>"><br />
-        <button type="submit" name="update-comment">Update</button>
+
+        <div class="mb-3">
+            <label for="body" class="form-label">Edit comment</label>
+            <input type="text" class="form-control" name="body" value="<?= $comment['body'] ?>"><br />
+        </div>
+        <button type="submit" name="update-comment" class="btn btn-primary">Edit</button>
     </form>
-</body>
+</div>
